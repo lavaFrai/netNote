a5ef6842abd85ccd9df2f24ebe0035df5529800f
lavaFrai <vladimirkurdiukovv@yandex.ru>
2021-12-17 22:36:44 +0300

    Hey, thats NOTEPAD!!!111!1! :)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,126 @@ namespace windows
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.open_file = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.ToolStripMenuItem();
+            this.save_as = new System.Windows.Forms.ToolStripMenuItem();
+            this.user_icon = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textArea = new System.Windows.Forms.RichTextBox();
+            this.new_file = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.user_icon)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 570);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(300, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.new_file,
+            this.open_file,
+            this.save,
+            this.save_as});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // open_file
+            // 
+            this.open_file.Name = "open_file";
+            this.open_file.Size = new System.Drawing.Size(180, 22);
+            this.open_file.Text = "Открыть";
+            this.open_file.Click += new System.EventHandler(this.open_file_Click);
+            // 
+            // save
+            // 
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(180, 22);
+            this.save.Text = "Сохранить";
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // save_as
+            // 
+            this.save_as.Name = "save_as";
+            this.save_as.Size = new System.Drawing.Size(180, 22);
+            this.save_as.Text = "Сохранить как...";
+            this.save_as.Click += new System.EventHandler(this.save_as_Click);
+            // 
+            // user_icon
+            // 
+            this.user_icon.BackgroundImage = global::windows.Properties.Resources.user_icon;
+            this.user_icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.user_icon.Image = global::windows.Properties.Resources.user_icon_dark;
+            this.user_icon.Location = new System.Drawing.Point(450, 10);
+            this.user_icon.Name = "user_icon";
+            this.user_icon.Size = new System.Drawing.Size(30, 30);
+            this.user_icon.TabIndex = 1;
+            this.user_icon.TabStop = false;
+            this.user_icon.Click += new System.EventHandler(this.user_icon_Click);
+            this.user_icon.MouseEnter += new System.EventHandler(this.user_icon_MouseEnter);
+            this.user_icon.MouseLeave += new System.EventHandler(this.user_icon_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.user_icon);
+            this.panel2.Location = new System.Drawing.Point(300, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(500, 50);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Controls.Add(this.textArea);
+            this.panel3.Location = new System.Drawing.Point(300, 50);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(500, 550);
+            this.panel3.TabIndex = 1;
+            // 
+            // textArea
+            // 
+            this.textArea.BackColor = System.Drawing.SystemColors.Control;
+            this.textArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textArea.EnableAutoDragDrop = true;
+            this.textArea.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textArea.Location = new System.Drawing.Point(7, 10);
+            this.textArea.Margin = new System.Windows.Forms.Padding(3, 10, 3, 3);
+            this.textArea.Name = "textArea";
+            this.textArea.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.textArea.Size = new System.Drawing.Size(477, 489);
+            this.textArea.TabIndex = 0;
+            this.textArea.Text = "";
+            // 
+            // new_file
+            // 
+            this.new_file.Name = "new_file";
+            this.new_file.Size = new System.Drawing.Size(180, 22);
+            this.new_file.Text = "Создать";
+            this.new_file.Click += new System.EventHandler(this.new_file_Click);
             // 
             // Form1
             // 
@@ -37,17 +156,39 @@ namespace windows
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.user_icon)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox user_icon;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox textArea;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open_file;
+        private System.Windows.Forms.ToolStripMenuItem save;
+        private System.Windows.Forms.ToolStripMenuItem save_as;
+        private System.Windows.Forms.ToolStripMenuItem new_file;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,88 @@ namespace windows
 {
     public partial class Form1 : Form
     {
+        private string _path = "Null";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void user_icon_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Debug information message");
+        }
+
+        private void user_icon_MouseEnter(object sender, EventArgs e)
+        {
+            user_icon.BackgroundImage = Properties.Resources.user_icon_dark;
+        }
+
+        private void user_icon_MouseLeave(object sender, EventArgs e)
+        {
+            user_icon.BackgroundImage = Properties.Resources.user_icon;
+        }
+
+        private void open_file_Click(object sender, EventArgs e)
+        {
+            string path;
+            string fileContent;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = openFileDialog.FileName;
+                    _path = path;
+                    fileContent = File.ReadAllText(_path, Encoding.Unicode);
+                    textArea.Text = fileContent;
+                }
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (_path == "Null")
+            {
+                save_as_Click(sender, e);
+            }
+            else
+            {
+                using (FileStream fstream = new FileStream(_path, FileMode.OpenOrCreate))
+                {
+                    fstream.Write(Encoding.Unicode.GetBytes(textArea.Text), 0, Encoding.Unicode.GetBytes(textArea.Text).Length);
+                    fstream.Close();
+                }
+            }
+        }
+
+        private void save_as_Click(object sender, EventArgs e)
+        {
+            string path;
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = "c:\\";
+                saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 2;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = saveFileDialog.FileName;
+                    _path = path;
+                    save_Click(sender, e);
+                }
+            }
+        }
+
+        private void new_file_Click(object sender, EventArgs e)
+        {
+            _path = "Null";
+            textArea.Text = "";
         }
     }
 }
